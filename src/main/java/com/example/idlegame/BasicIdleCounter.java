cab55571fe644e261c3aa73f4f602ad3b48516f7
Nguyentt74 <43280328+Nguyentt74@users.noreply.github.com>
2021-12-11 23:05:59 -0500

    Update BasicIdleCounter.java
    Added decrement variables and functions. Have not implemented them in the main activity yet

--- src/main/java/com/example/idlegame/BasicIdleCounter.java
+++ src/main/java/com/example/idlegame/BasicIdleCounter.java
@@ -6,16 +6,21 @@ public class BasicIdleCounter {
     public int attack = 0;
     public int defense = 0;
     private int increment = 1;
+    private int decrement;
     public long getMoney() {
         return money;
     }
     public void setIncrement(int num){
         increment = num;
     }
+    public void setDecrement(int num1) {decrement = num1}
     public void incrementMoney(){
     //Increments the money counter each time
            money = money + increment;
         }
+    public void decrementMoney(){
+        money = money - decrement;
+    }
     public void incrementAttack(){
 
         attack++;
