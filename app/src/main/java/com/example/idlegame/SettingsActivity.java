9afc6570dbb3e357645805d80beb2ad79e2e7b55
Nguyentt74 <43280328+Nguyentt74@users.noreply.github.com>
2021-12-13 14:35:04 -0500

    Attempt to fix build #2
    Added additional missing files

---
+++ app/src/main/java/com/example/idlegame/SettingsActivity.java
@@ -0,0 +1,48 @@
+package com.example.idlegame;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+    long lastDown;
+    long lastDuration;
+    Button RandomButton;
+    ImageButton settingsButton;
+
+    //TextView boop;
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        //RandomButton = (Button) findViewById(R.id.button4);
+/*
+                settingsButton.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                            lastDown = System.currentTimeMillis();
+                            //startActivity(new Intent(SettingsActivity.this, MainActivity.class));
+
+                        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                            lastDuration = System.currentTimeMillis() - lastDown;
+                        }
+
+                        return true;
+                    }
+                });
+*/
+
+        }
+        //handler.post(new MyRunnable(handler, boop);
+    }
+
